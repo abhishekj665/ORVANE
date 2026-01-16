db2e2b7804d0a382aca327f5e875f634683e6e39
abhishekj665 <jeveneabhi665@gmail.com>
2026-01-16 18:25:53 +0530

    Refactor code formatting and improve validation feedback in forms

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
-console.log(process.env.ATLAS_URL);
 
 const express = require("express");
+
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
@@ -32,47 +32,49 @@ const serverless = require("serverless-http");
 const db_URL = process.env.ATLAS_URL;
 
 main()
-.then(() => console.log("Connected to DB"))
-.catch((err) => console.log(err));
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(db_URL);
 }
 
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended : true}));
-app.use(methodOverride(function (req, res) {
-  if (req.query && typeof req.query._method === 'string') {
-    return req.query._method;
-  }
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  methodOverride(function (req, res) {
+    if (req.query && typeof req.query._method === "string") {
+      return req.query._method;
+    }
+  })
+);
 
-app.engine("ejs", ejsMate)
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 const store = MongoStore.create({
-  mongoUrl : db_URL,
-  crypto : {
-    secret : process.env.SECRET
+  mongoUrl: db_URL,
+  crypto: {
+    secret: process.env.SECRET,
   },
-  touchAfter : 24 * 3600
+  touchAfter: 24 * 3600,
 });
 
 store.on("error", () => {
-  console.log("Error in Mongo Session Store", err)
+  console.log("Error in Mongo Session Store", err);
 });
 
 const sessionOptions = {
   store,
-  secret : "musupersecretcode",
-  resave : false,
-  saveUninitialized : false,
-  cookie : {
-    expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge : 7 * 24 * 60 * 60 * 1000,
-    httpOnly : true,
-  }
+  secret: "musupersecretcode",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.use(session(sessionOptions));
@@ -85,16 +87,16 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.user = req.user;
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
+  res.locals.user = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 app.use("/about", aboutRouter);
-app.use("/orvane",orvaneRouter);
-app.use("/user",userRouter);
-app.use("/portfolio",portfolioRouter );
+app.use("/orvane", orvaneRouter);
+app.use("/user", userRouter);
+app.use("/portfolio", portfolioRouter);
 app.use("/employee", employeeRouter);
 app.use("/service", serviceRouter);
 
@@ -104,16 +106,16 @@ app.get("/", (req, res) => {
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
-})
+});
 
 app.use((err, req, res, next) => {
-  let {statusCode = 500, message = "Something went wrong"} = err;
-  res.render("error.ejs",{message});
-})
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.render("error.ejs", { message });
+});
 
 app.listen(8080, () => {
-    console.log("Listening Successfuly");
-})
+  console.log("Listening Successfuly");
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
